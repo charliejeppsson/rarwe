c433b2f90e6aa163c3f8379f905ac24fd7317f91
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-20 02:18:20 +0200

    add controller event handlers and improve UI for creating songs and bands

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,12 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
+
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
+
     updateRating: function(params) {
       var song = params.item,
         rating = params.rating;
 
       song.set('rating', rating);
     }
-  }
+  },
+
+  title: '',
+
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  })
+
 });
