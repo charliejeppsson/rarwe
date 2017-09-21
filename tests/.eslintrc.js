bea58f92670e1708c003a2229d8bc026b11ada24
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-21 15:08:16 +0200

    add tests for creating band and creating songs along with associated test helpers

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   env: {
     embertest: true
+  },
+  globals: {
+    assertTrimmedText: false,
+    assertLength: false,
+    assertElement: false,
   }
 };
--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,15 +1,19 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
+import './asserts';
 
 export default function startApp(attrs) {
+  let application;
+
   let attributes = Ember.merge({}, config.APP);
   attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
-  return Ember.run(() => {
-    let application = Application.create(attributes);
+  Ember.run(() => {
+    application = Application.create(attributes);
     application.setupForTesting();
     application.injectTestHelpers();
-    return application;
   });
+
+  return application;
 }
