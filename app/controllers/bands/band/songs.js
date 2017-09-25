d176335ab22cf5f96442b6c6f0b295b95e437f18
Charlie Jeppsson <charlie.jeppsson1@gmail.com>
2017-09-25 17:01:49 +0200

    add sorting and searching functionality

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -21,10 +21,35 @@ export default Ember.Controller.extend({
       }
       song.set('rating', rating);
       return song.save();
+    },
+
+    setSorting: function(option) {
+      this.set('sortBy', option);
     }
   },
 
+  searchTerm: '',
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+
   title: '',
+
+  sortBy: 'ratingDesc',
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      'ratingDesc': 'rating:desc,title:asc',
+      'ratingAsc': 'rating:asc,title:asc',
+      'titleDesc': 'title:desc',
+      'titleAsc': 'title:asc',
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
 
   isAddButtonDisabled: Ember.computed('title', function() {
     return Ember.isEmpty(this.get('title'));
